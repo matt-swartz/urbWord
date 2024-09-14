90ff6757169933b8d73ce2134fa5dc3117cf6172
matt-swartz <mattcswartz@gmail.com>
2024-09-14 14:48:43 -0400

    urbWord source code
    The urbWord logic

---
+++ script.js
@@ -0,0 +1,238 @@
+import { WORDS } from "./words.js";
+
+const NUMBER_OF_GUESSES = 5;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let letterColors = [];
+var nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+
+console.log(rightGuessString);
+
+function initBoard() {
+  let board = document.getElementById("game-board");
+
+  for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+    let row = document.createElement("div");
+    row.className = "letter-row";
+
+    for (let j = 0; j < 5; j++) {
+      let box = document.createElement("div");
+      box.className = "letter-box";
+      row.appendChild(box);
+    }
+
+    board.appendChild(row);
+  }
+}
+
+function shadeKeyBoard(letter, color) {
+  for (const elem of document.getElementsByClassName("keyboard-button")) {
+    if (elem.textContent === letter) {
+      let oldColor = elem.style.backgroundColor;
+      if (oldColor === "green") {
+        return;
+      }
+
+      if (oldColor === "yellow" && color !== "green") {
+        return;
+      }
+
+      elem.style.backgroundColor = color;
+      break;
+    }
+  }
+}
+
+function deleteLetter() {
+  let row = document.getElementsByClassName("letter-row")[5 - guessesRemaining];
+  let box = row.children[nextLetter - 1];
+  box.textContent = "";
+  box.classList.remove("filled-box");
+  currentGuess.pop();
+  nextLetter -= 1;
+}
+
+function checkGuess() {
+  let row = document.getElementsByClassName("letter-row")[5 - guessesRemaining];
+  let guessString = "";
+  let rightGuess = Array.from(rightGuessString);
+
+  for (const val of currentGuess) {
+    guessString += val;
+  }
+
+  if (guessString.length != 5) {
+    toastr.error("Not enough letters!");
+    return;
+  }
+
+  if (!WORDS.includes(guessString)) {
+    toastr.error("Word not in list!");
+    return;
+  }
+
+  var letterColor = ["gray", "gray", "gray", "gray", "gray"];
+
+  //check green
+  for (let i = 0; i < 5; i++) {
+    if (rightGuess[i] == currentGuess[i]) {
+      letterColor[i] = "green";
+      rightGuess[i] = "#";
+    }
+  }
+
+  //check yellow
+  //checking guess letters
+  for (let i = 0; i < 5; i++) {
+    if (letterColor[i] == "green") continue;
+
+    //checking right letters
+    for (let j = 0; j < 5; j++) {
+      if (rightGuess[j] == currentGuess[i]) {
+        letterColor[i] = "gold";
+        rightGuess[j] = "#";
+      }
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    let box = row.children[i];
+    let delay = 250 * i;
+    setTimeout(() => {
+      //flip box
+      animateCSS(box, "flipInX");
+      //shade box
+      box.style.backgroundColor = letterColor[i];
+      box.style.borderColor = letterColor[i];
+      shadeKeyBoard(guessString.charAt(i) + "", letterColor[i]);
+    }, delay);
+  }
+  letterColors.push(letterColor);
+  if (guessString === rightGuessString) {
+    toastr.success("You guessed right! Game over!");
+    recordGameResult(true, 6 - guessesRemaining);
+    guessesRemaining = 0;
+    return;
+  } else {
+    guessesRemaining -= 1;
+    currentGuess = [];
+    nextLetter = 0;
+
+    if (guessesRemaining === 0) {
+      toastr.error("You've run out of guesses! Game over!");
+      toastr.info(`The right word was: "${rightGuessString}"`);
+      recordGameResult(false, 0);
+    }
+  }
+}
+
+function insertLetter(pressedKey) {
+  if (nextLetter === 5) {
+    return;
+  }
+  pressedKey = pressedKey.toLowerCase();
+
+  let row = document.getElementsByClassName("letter-row")[5 - guessesRemaining];
+  let box = row.children[nextLetter];
+  animateCSS(box, "pulse");
+  box.textContent = pressedKey;
+  box.classList.add("filled-box");
+  currentGuess.push(pressedKey);
+  nextLetter += 1;
+}
+
+// Update the users stats
+function recordGameResult(isWin, guessNumber) {
+  // Retrieve stats from localStorage or initialize if not present
+  var stats = JSON.parse(localStorage.getItem('gameStats')) || {
+      gamesPlayed: 0,
+      wins: 0,
+      currentStreak: 0,
+      maxStreak: 0,
+      guessDistribution: [0, 0, 0, 0, 0, 0]
+  };
+
+  // Update stats based on game result
+  stats.gamesPlayed++;
+  if (isWin) {
+      stats.wins++;
+      stats.currentStreak++;
+      stats.maxStreak = Math.max(stats.maxStreak, stats.currentStreak);
+      stats.guessDistribution[guessNumber - 1] += 1
+  } else {
+      stats.currentStreak = 0;
+  }
+
+  // Save updated stats to localStorage
+  localStorage.setItem('gameStats', JSON.stringify(stats));
+  localStorage.setItem('guessNumber', JSON.stringify(guessNumber));
+  localStorage.setItem('letterColors', JSON.stringify(letterColors));
+  localStorage.setItem('wordOfDay', JSON.stringify(rightGuessString));
+  
+  // Go to the stats page
+  setTimeout(function() {
+    window.location.href = "statistics.html?word=" + rightGuessString;
+  }, 2500);
+}
+
+const animateCSS = (element, animation, prefix = "animate__") =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    // const node = document.querySelector(element);
+    const node = element;
+    node.style.setProperty("--animate-duration", "0.3s");
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
+
+document.addEventListener("keyup", (e) => {
+  if (guessesRemaining === 0) {
+    return;
+  }
+
+  let pressedKey = String(e.key);
+  if (pressedKey === "Backspace" && nextLetter !== 0) {
+    deleteLetter();
+    return;
+  }
+
+  if (pressedKey === "Enter") {
+    checkGuess();
+    return;
+  }
+
+  let found = pressedKey.match(/[a-z]/gi);
+  if (!found || found.length > 1) {
+    return;
+  } else {
+    insertLetter(pressedKey);
+  }
+});
+
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (!target.classList.contains("keyboard-button")) {
+    return;
+  }
+  let key = target.textContent;
+
+  if (key === "Del") {
+    key = "Backspace";
+  }
+
+  document.dispatchEvent(new KeyboardEvent("keyup", { key: key }));
+});
+
+initBoard();
---
+++ stats.js
@@ -0,0 +1,88 @@
+// Retrieve stats from localStorage or initialize if not present
+var stats = JSON.parse(localStorage.getItem('gameStats')) || {
+    gamesPlayed: 0,
+    wins: 0,
+    currentStreak: 0,
+    maxStreak: 0,
+    guessDistribution: [0, 0, 0, 0, 0, 0]
+};
+
+// Get the share button
+const shareButton = document.getElementById('shareButton');
+
+function initStats() {
+    // Update stats
+
+    // Get the <th> element by its ID
+    var gamesPlayed = document.getElementById('played');
+    var winPercentage = document.getElementById('wins');
+    var currentStreak = document.getElementById('streak');
+    var maxStreak = document.getElementById('maxstreak');
+    var firstGuess = document.getElementById('firstguess');
+    var secondGuess = document.getElementById('secondguess');
+    var thirdGuess = document.getElementById('thirdguess');
+    var fourthGuess = document.getElementById('fourthguess');
+    var fifthGuess = document.getElementById('fifthguess');
+    var sixthGuess = document.getElementById('sixthguess');
+
+    // Update the text content of the <th> element
+    gamesPlayed.textContent = stats.gamesPlayed.toString();
+    winPercentage.textContent = (stats.wins/stats.gamesPlayed * 100).toString();
+    currentStreak.textContent = stats.currentStreak.toString();
+    maxStreak.textContent = stats.maxStreak.toString();
+    // firstGuess.textContent = '1: ${firstGuess} game(s)';
+    firstGuess.textContent = '1: ' + stats.guessDistribution[0].toString() + ' game(s)';
+    secondGuess.textContent = '2: ' + stats.guessDistribution[1].toString() + ' game(s)';
+    thirdGuess.textContent = '3: ' + stats.guessDistribution[2].toString() + ' game(s)';
+    fourthGuess.textContent = '4: ' + stats.guessDistribution[3].toString() + ' game(s)';
+    fifthGuess.textContent = '5: ' + stats.guessDistribution[4].toString() + ' game(s)';
+    sixthGuess.textContent = '6: ' + stats.guessDistribution[5].toString() + ' game(s)';
+}
+
+// Add click event listener to the copy button
+shareButton.addEventListener('click', async () => {
+    try {
+        // Get the stats for the share button
+        const game = stats.gamesPlayed.toString();
+        const guess = JSON.parse(localStorage.getItem('guessNumber'));
+        const letterColors = JSON.parse(localStorage.getItem('letterColors'));
+        const wordOfDay = JSON.parse(localStorage.getItem('wordOfDay')).toUpperCase();
+
+        // Our variables for the Guess Distribution string
+        var guessDistributionString = "";
+        var guesses = 0;
+        letterColors.forEach(subArray => {
+            subArray.forEach(element => {
+                if (element === "gray") {
+                    guessDistributionString += "⬛"
+                } else if (element === "gold") {
+                    guessDistributionString += "🟨"
+                } else if (element === "green") {
+                    guessDistributionString += "🟩"
+                }
+            });
+            guessDistributionString += "\n";
+            guesses += 1
+        });
+
+        // Add the additional black rows
+        if (guesses < 6) {
+            const additionalRows = 6 - guesses;
+            const blackRow = "⬛⬛⬛⬛⬛\n";
+            const blackRows = blackRow.repeat(additionalRows);
+            guessDistributionString += blackRows;
+        }
+
+        // Create the stats string
+        const definitionURL = "https://www.urbandictionary.com/define.php?term=" + wordOfDay
+        const statsString = "urbWord of the Day " + game.toString() + " " + guess.toString() + " / 6 \n" + guessDistributionString + wordOfDay + " Definition: " + definitionURL;
+
+        // Add to the users clipboard
+        await navigator.clipboard.writeText(statsString);
+        alert('Text copied to clipboard!');
+    } catch (err) {
+        console.error('Failed to copy text: ', err);
+    }
+});
+
+initStats();
